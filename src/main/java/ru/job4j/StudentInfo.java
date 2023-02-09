997432e1dd5820a22aaa6906883bd19fa46de790
Your name <you@example.com>
2023-02-10 01:23:34 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Nikita Migushev");
+        System.out.println("09.10.1987");
+    }
+}
