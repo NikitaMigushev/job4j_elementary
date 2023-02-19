d78ed15b01192c1714967de8aac5c1059dd5e5d7
Your name <you@example.com>
2023-02-19 19:58:05 +0300

    1. Модульные тесты. [#159435]

---
+++ src/test/java/ru/job4j/ConverterTest.java
@@ -0,0 +1,25 @@
+package ru.job4j;
+
+import org.junit.Test;
+import org.junit.Assert;
+
+public class ConverterTest {
+
+    @Test
+    public void whenConvert140RblThen2Euro() {
+        float in = 140;
+        float expected = 2;
+        float out = Converter.rubleToEuro(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
+    }
+
+    @Test
+    public void whenConvert140RblThen2dot3333() {
+        float in = 180;
+        float expected = 3;
+        float out = Converter.rubbleToDollar(in);
+        float eps = 0.0001f;
+        Assert.assertEquals(expected, out, eps);
+    }
+}
