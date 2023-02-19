aff9080ffe276ad8b53d7550398c1c8f1c96ebde
Your name <you@example.com>
2023-02-19 14:20:49 +0300

    0. Что такое тестирование [#159431]

--- src/main/java/ru/job4j/Converter.java
+++ src/main/java/ru/job4j/Converter.java
@@ -14,9 +14,21 @@ public class Converter {
 
     public static void main(String[] args) {
         float euro = Converter.rubleToEuro(140);
-        System.out.println("140 rubles are " + euro + " euro.");
+        System.out.println("140 rubbles are " + euro + " euro.");
         float dollar = Converter.rubbleToDollar(140);
-        System.out.println("140 rubles are " + dollar + " dollar.");
+        System.out.println("140 rubbles are " + dollar + " dollar.");
+
+        float inForTestRubbleToEuro = 140;
+        float expectedForTestRubbleToEuro = 2;
+        float outForTestRubbleToEuro = Converter.rubleToEuro(inForTestRubbleToEuro);
+        boolean passedForTestRubbleToEuro = expectedForTestRubbleToEuro == outForTestRubbleToEuro;
+        System.out.println("140 rubbles are 2. Test result: " + passedForTestRubbleToEuro);
+
+        float inForTestRubbleToDollar = 160;
+        float expectedForTestRubbleToDollar = inForTestRubbleToDollar / 60;
+        float outForTestRubbleToDollar = Converter.rubbleToDollar(inForTestRubbleToDollar);
+        boolean passedForTestRubbleToDollar = expectedForTestRubbleToDollar == outForTestRubbleToDollar;
+        System.out.println("160 rubbles are " + expectedForTestRubbleToDollar + " dollar. Test result: " + passedForTestRubbleToDollar);
     }
 }
 
