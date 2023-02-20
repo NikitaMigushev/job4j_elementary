4a3cd061900c650ea106e7070297d94207259aa6
Your name <you@example.com>
2023-02-20 20:38:55 +0300

    4.1.0. Оператор if с блоком else [#227534]

---
+++ src/main/java/ru/job4j/condition/Tour.java
@@ -0,0 +1,18 @@
+package ru.job4j.condition;
+
+public class Tour {
+
+    public static void offer(boolean passpport) {
+        System.out.println("A client has foreign passport: " + passpport);
+        if (passpport) {
+            System.out.println("Tours abroad.");
+        } else {
+            System.out.println("Tours in Russia.");
+        }
+    }
+
+    public static void main(String[] args) {
+        Tour.offer(true);
+        Tour.offer(false);
+    }
+}
