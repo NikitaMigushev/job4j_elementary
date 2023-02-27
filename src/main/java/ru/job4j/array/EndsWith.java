6310100e049bc4fae3bdabc46b5f56e949bdb748
Your name <you@example.com>
2023-02-27 11:09:29 +0300

    6.4.3. Слово заканчивается на ... [#156317 #395409]

--- src/main/java/ru/job4j/array/EndsWith.java
+++ src/main/java/ru/job4j/array/EndsWith.java
@@ -4,8 +4,7 @@ public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
         for (int i = 0; i < post.length; i++) {
-            int start = word.length - post.length + i;
-            if (word[start] != post[i]) {
+            if (word[word.length - i - 1] != post[post.length - i - 1]) {
                 result = false;
                 break;
             }
